6d75ad23a47b6739ad14ff37a97abb1e93d25f3a
zeimedee <42755623+zeimedee@users.noreply.github.com>
2023-03-09 11:50:50 +0000

    switch2zero backend summary

--- controllers/simulationController.js
+++ controllers/simulationController.js
@@ -4,9 +4,16 @@ const SimulationController = async (req, res) => {
     const { data } = req.body
     const result = calculateInputs(data)
     const results = calculateCarbonOffset(result.trees, result.consumption, result.date);
-    
+    const l = results.offsetData.length
+    const summary = {
+        year:results.offsetData[l-1].month,
+        years:results.offsetData.length/12,
+        total:results.spendData[l-1].amt,
+        trees:result.trees
+    }
     return res.send({
-        results
+        results,
+        summary
 
     })
 }
